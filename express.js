ad2fecdf5bad9f9bb6a0222a00b88aade3a2ed2f
o.sabri <o.sabri@sct-telecom.fr>
2021-09-23 16:35:31 +0100

    "first section done"

--- express.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = createApplication;
-
-function createApplication() {
-    let app = function(req,res,next) {
-    };
-
-    return app;
-    //This returned app is what end user will get when they call express()
-}
