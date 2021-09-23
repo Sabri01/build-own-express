9c0c6257ec4e0dec2b905fd70f9b6a7612d0e5e2
o.sabri <o.sabri@sct-telecom.fr>
2021-09-23 14:15:06 +0100

    first commit

---
+++ express.js
@@ -0,0 +1,9 @@
+exports = module.exports = createApplication;
+
+function createApplication() {
+    var app = function(req,res,next) {
+    };
+
+    return app;
+    //This returned app is what end user will get when they call express()
+}
